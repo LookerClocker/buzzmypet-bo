88929e9b7a2bcdb399dddcb8ff8391be6b4cdc1a
Olexandr Kushnir <olexandrrazor@mail.ru>
2016-10-13 10:32:48 +0300

    Finished component wich fetch users

--- src/App.js
+++ src/App.js
@@ -6,72 +6,17 @@ import UserTable from './UserTable';
 // import MenuItem  from 'material-ui/MenuItem';
 // import ThemeManager  from 'material-ui/styles/MuiThemeProvider';
 
-var Parse = require('parse').Parse;
-
-var parseApplicationId = 'KzykKl3uejlA8eNvij0wbc45SS6XaZPqZM3FsIeV';
-var parseJavaScriptKey = 'mplYkntmCwoNEhmH2uuuRPeRosulwSJwxtOqs1gh';
-var parseMasterKey = 'r0J0D2CYbm6Gkilx2IdWO4tOGO2MrVe3SHVjD401';
-
-Parse.initialize(parseApplicationId, parseJavaScriptKey, parseMasterKey);
-
 export default class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            usersList: [],
-            showUsers: false
-        };
-    };
-    // componentDidMount () {
-    //     var _this = this;
-    //
-    //     this.getUsers(function (items) {
-    //         _this.setState({
-    //             usersList: items.map(function(user){
-    //                 return user.get('name');
-    //             })
-    //         });
-    //     });
-    // };
-
-    getUsers(callback) {
-        console.log('log');
-        var self = this;
-        var query = new Parse.Query('User');
-        query.limit(10000);
-        query.find({
-            success: function (users) {
-                console.log('before func-> ',this.state.usersList);
-                self.setState({
-                    showUsers: !self.state.showUsers,
-                    usersList: users.map(function(user) {
-                        return user.get('name');
-                    })
-                });
-                callback(users);
-            },
-            error: function (error) {
-                console.error('getUser() error', error);
-                callback(null, error);
-            }
-        });
+        this.state = {};
     };
 
     render() {
-        console.log('user array -> ',this.state.usersList);
-        console.log('user show -> ',this.state.showUsers);
-        var container;
-        if(this.state.showUsers) {
-            container = <UserTable userList={this.state.usersList}/>;
-        }
-        else {
-            container='bla';
-        }
 
         return (
             <div>
-                <button onClick={this.getUsers}>get users</button>
-                {container}
+                <UserTable/>
             </div>
         );
     }
--- src/UserTable.js
+++ src/UserTable.js
@@ -2,18 +2,22 @@ import React, {Component} from 'react';
 import ReactDataGrid from 'react-data-grid';
 import Json2csv from 'json2csv';
 
-// var Parse = require('parse').Parse;
-//
-// var parseApplicationId = 'KzykKl3uejlA8eNvij0wbc45SS6XaZPqZM3FsIeV';
-// var parseJavaScriptKey = 'mplYkntmCwoNEhmH2uuuRPeRosulwSJwxtOqs1gh';
-// var parseMasterKey = 'r0J0D2CYbm6Gkilx2IdWO4tOGO2MrVe3SHVjD401';
-//
-// Parse.initialize(parseApplicationId, parseJavaScriptKey, parseMasterKey);
+var Parse = require('parse').Parse;
+
+var parseApplicationId = 'KzykKl3uejlA8eNvij0wbc45SS6XaZPqZM3FsIeV';
+var parseJavaScriptKey = 'mplYkntmCwoNEhmH2uuuRPeRosulwSJwxtOqs1gh';
+var parseMasterKey = 'r0J0D2CYbm6Gkilx2IdWO4tOGO2MrVe3SHVjD401';
+
+Parse.initialize(parseApplicationId, parseJavaScriptKey, parseMasterKey);
 
 var columns = [
     {
         key: 'name',
         name: 'NAME',
+    },
+    {
+        key: 'email',
+        name: 'EMAIL'
     }
 ];
 
@@ -21,20 +25,64 @@ export default class UserTable extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            user:this.props.userList
+            usersList: []
         };
 
         this.rowGetter = this.rowGetter.bind(this);
         this.toCSV = this.toCSV.bind(this);
     };
 
+    componentDidMount() {
+        var _this = this;
+
+        this.getUsers(function (items) {
+            console.log('This is items-> ', items);
+            _this.setState({
+                usersList: items
+            });
+            console.log('This is userListAfter -> ', _this.state.usersList + ' or just with this-> ' + this.state.usersList);
+        });
+    };
+
+    getUsers(callback) {
+        var _this = this;
+
+        var query = new Parse.Query('User');
+        query.limit(10000);
+        query.find({
+            success: function (users) {
+                console.log('before func-> ', _this.state.usersList);
+                _this.setState({
+                    usersList: users.map(function (user) {
+                        return {
+                            name: user.get('name'),
+                            email: user.get('email')
+                        }
+                    })
+                });
+                callback(users);
+            },
+            error: function (error) {
+                console.error('getUser() error', error);
+                callback(null, error);
+            }
+        });
+    };
+
     rowGetter(i) {
-        return this.state.user[i];
+        return this.state.usersList[i];
     };
 
     toCSV() {
-        var fields = ['id', 'name', 'email'];
-        var dataToCsv = this.state.user;
+        var fields = ['name', 'email'];
+        var dataToCsv = this.state.usersList.map(function (user) {
+            return {
+                name: user.get('name'),
+                email: user.get('email')
+            }
+        });
+
+        console.log('data to csv-> ', dataToCsv);
 
         const filename = 'Users.csv';
         const csv = Json2csv({data: dataToCsv, fields: fields});
@@ -62,7 +110,6 @@ export default class UserTable extends Component {
     render() {
         return (
             <div>
-                <p>jhkj jkjkhkjhk</p>
                 <button
                     className="col-md-offset-10 btn btn-success"
                     onClick={this.toCSV}>
@@ -70,17 +117,13 @@ export default class UserTable extends Component {
                 </button>
                 <ReactDataGrid
                     idProperty="id"
-                    dataSource={this.state.user}
+                    dataSource={this.state.usersList}
                     columns={columns}
                     rowGetter={this.rowGetter}
-                    rowsCount={this.state.user.length}
+                    rowsCount={this.state.usersList.length}
                     minHeight={400}/>
             </div>
 
         )
     };
-};
-
-UserTable.propTypes = {
-    userList: React.PropTypes.array.isRequired
 };
