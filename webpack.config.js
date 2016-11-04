4d023e754cea799f8f3a8119e053270d936f1275
Olexandr Kushnir <olexandrrazor@mail.ru>
2016-11-04 19:14:16 +0200

    Fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,48 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'app', 'app.js'),
+    entry: path.resolve(__dirname, 'src', 'App.js'),
     output: {
-        filename: 'app.js',
+        filename: 'App.js',
         path: path.resolve(__dirname, 'public/build'),
         publicPath: path.resolve(__dirname, 'public/build')
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
     },
 
     module: {
         loaders: [
+            // {
+            //     exclude: /node_modules/,
+            //     loader: 'babel',
+            //     test: /\.js$/,
+            // },
             {
+                test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loader: 'babel',
-                test: /\.js$/,
+                loader: "babel",
+                query: {
+                    presets: ["es2015", "stage-0", "react"]
+                }
+
             },
+
+            {
+                test: /\.html$/,
+                loader: "html"
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
+                loader: 'url-loader?limit=10000'
+            }, {
+                test: /\.(eot|ttf|wav|mp3)$/,
+                loader: 'file-loader'
+            }
         ]
     }
 };
