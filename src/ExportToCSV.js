bca3745a05fd82e7a4a2abf9fe5b71de947687a2
Olexandr Kushnir <olexandrrazor@mail.ru>
2016-11-08 12:18:48 +0200

    Removed json2csv

--- src/ExportToCSV.js
+++ src/ExportToCSV.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Json2csv from 'json2csv';
+// import Json2csv from 'json2csv';
 import FlatButton from 'material-ui/FlatButton';
 
 import PubSub from 'pubsub-js';
@@ -26,30 +26,30 @@ export default class ExportToCSV extends Component {
     };
 
     toCSV = ()=> {
-        var fields = Object.keys(this.state.rows[0]);
-
-        const filename = this.props.path + '.csv';
-        const csv = Json2csv({data: this.state.rows, fields: fields});
-
-        var blob = new Blob([csv], {type: 'text/csv;charset=utf-8;'});
-        if (navigator.msSaveBlob) {
-            navigator.msSaveBlob(blob, filename);
-        }
-        else {
-            var link = document.createElement('a');
-            if (link.download !== undefined) {
-                var url = URL.createObjectURL(blob);
-                link.setAttribute('href', url);
-                link.setAttribute('download', filename);
-                link.style.visibility = 'hidden';
-                document.body.appendChild(link);
-                link.click();
-                document.body.removeChild(link);
-            }
-        }
-        this.setState({
-            rows: []
-        })
+    //     var fields = Object.keys(this.state.rows[0]);
+    //
+    //     const filename = this.props.path + '.csv';
+    //     const csv = Json2csv({data: this.state.rows, fields: fields});
+    //
+    //     var blob = new Blob([csv], {type: 'text/csv;charset=utf-8;'});
+    //     if (navigator.msSaveBlob) {
+    //         navigator.msSaveBlob(blob, filename);
+    //     }
+    //     else {
+    //         var link = document.createElement('a');
+    //         if (link.download !== undefined) {
+    //             var url = URL.createObjectURL(blob);
+    //             link.setAttribute('href', url);
+    //             link.setAttribute('download', filename);
+    //             link.style.visibility = 'hidden';
+    //             document.body.appendChild(link);
+    //             link.click();
+    //             document.body.removeChild(link);
+    //         }
+    //     }
+    //     this.setState({
+    //         rows: []
+    //     })
     };
 
     render() {
