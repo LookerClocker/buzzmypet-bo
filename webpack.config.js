2bcd9a29da00664e6467e2960cce703515db96b6
Olexandr Kushnir <olexandrrazor@mail.ru>
2016-11-09 17:01:26 +0200

    Added fallback

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,12 @@ module.exports = {
         fs: "empty"
     },
 
+    devServer: {
+        historyApiFallback: true,
+        contentBase: './',
+        hot: true
+    },
+
     module: {
         loaders: [
             {
@@ -32,14 +38,18 @@ module.exports = {
                 test: /\.html$/,
                 loader: "html"
             },
+
             {
                 test: /\.css$/,
                 loader: "style-loader!css-loader"
             },
+
             {
                 test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
                 loader: 'url-loader?limit=10000'
-            }, {
+            },
+
+            {
                 test: /\.(eot|ttf|wav|mp3)$/,
                 loader: 'file-loader'
             }
